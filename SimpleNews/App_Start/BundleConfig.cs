e2f3c0b75b124bccb807d51ae31a80ec4ec1dc5d
Emrah KARAMAN <ekaraman89@hotmail.com>
2018-05-29 00:31:36 +0300

    admin theme  added

--- SimpleNews/App_Start/BundleConfig.cs
+++ SimpleNews/App_Start/BundleConfig.cs
@@ -6,28 +6,59 @@ namespace SimpleNews
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/admin/styles")
-                .Include("~/Content/Style/bootstrap.css")
-                .Include("~/Content/Style/admin.css"));
 
-            bundles.Add(new StyleBundle("~/styles")
-                .Include("~/Content/Style/bootstrap.css")
-                .Include("~/Content/Style/site.css"));
+            #region admin layout styles
+            bundles.Add(new StyleBundle("~/admin/global-mandatory")
+                .Include("~/Areas/Admin/Content/assets/global/plugins/font-awesome/css/font-awesome.min.css")
+                .Include("~/Areas/Admin/Content/assets/global/plugins/simple-line-icons/simple-line-icons.min.css")
+                .Include("~/Areas/Admin/Content/assets/global/plugins/bootstrap/css/bootstrap.min.css")
+                .Include("~/Areas/Admin/Content/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css")
+            );
 
-            bundles.Add(new ScriptBundle("~/admin/scripts")
-                .Include("~/scripts/jquery-2.0.3.js")
-                .Include("~/scripts/jquery.validate.js")
-                .Include("~/scripts/jquery.validate.unobtrusive.js")
-                .Include("~/scripts/bootstrap.js"));
+            bundles.Add(new StyleBundle("~/admin/global")
+                .Include("~/Areas/Admin/Content/assets/global/css/components.min.css")
+                .Include("~/Areas/Admin/Content/assets/global/css/plugins.min.css")
+            );
 
-            bundles.Add(new ScriptBundle("~/scripts")
-                .Include("~/scripts/jquery-2.0.3.js")
-                .Include("~/scripts/jquery.validate.js")
-                .Include("~/scripts/jquery.validate.unobtrusive.js")
-                .Include("~/scripts/bootstrap.js"));
+            bundles.Add(new StyleBundle("~/admin/layout-style")
+                .Include("~/Areas/Admin/Content/assets/layouts/layout/css/layout.min.css")
+                .Include("~/Areas/Admin/Content/assets/layouts/layout/css/themes/darkblue.min.css")
+                .Include("~/Areas/Admin/Content/assets/layouts/layout/css/custom.min.css")
+            );
+            #endregion
+
+            #region admin layout scripts
+            bundles.Add(new ScriptBundle("~/admin/core-begin")
+                .Include("~/Areas/Admin/Content/assets/global/plugins/jquery.min.js")
+                .Include("~/Areas/Admin/Content/assets/global/plugins/bootstrap/js/bootstrap.min.js")
+                .Include("~/Areas/Admin/Content/assets/global/plugins/js.cookie.min.js")
+                .Include("~/Areas/Admin/Content/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js")
+                .Include("~/Areas/Admin/Content/assets/global/plugins/jquery.blockui.min.js")
+                .Include("~/Areas/Admin/Content/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js")
+            );
+
+            bundles.Add(new ScriptBundle("~/admin/global-scripts")
+                .Include("~/Areas/Admin/Content/assets/global/scripts/app.min.js")
+            );
+
+            bundles.Add(new ScriptBundle("~/admin/layout-scripts")
+                .Include("~/Areas/Admin/Content/assets/layouts/layout/scripts/layout.min.js")
+            );
+            #endregion
 
 
 
+            bundles.Add(new StyleBundle("~/admin/datatable-style")
+                .Include("~/Areas/Admin/Content/assets/global/plugins/datatables/datatables.min.css")
+                .Include("~/Areas/Admin/Content/assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css")
+            );
+
+            bundles.Add(new ScriptBundle("~/admin/datatable-script")
+                .Include("~/Areas/Admin/Content/assets/global/scripts/datatable.min.js")
+                .Include("~/Areas/Admin/Content/assets/global/plugins/datatables/datatables.min.js")
+                .Include("~/Areas/Admin/Content/assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js")
+                .Include("~/Areas/Admin/Content/assets/pages/scripts/table-datatables-buttons.js")
+                );
 
         }
     }
